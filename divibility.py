1bf38bb0667c9ab63f5ce55b1a31fc2b86747a01
Ashish Prasad <ashish.ap.rao@gmail.com>
2013-06-26 12:41:54 +0530

    added divisibility test, squares & cubes test

---
+++ divibility.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+import signal
+import sys
+
+cnt = 0
+score = 0
+
+def signal_handler(signal, frame):
+  print '\n' + '-'*80
+  print 'score = %d/%d' % (score, cnt)
+  print 'Bye !'
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+header = 'DIVISIBILITY TEST'
+print header
+print '-' * len(header)
+print '\nPress Ctrl+C to stop\n'
+
+n = int(raw_input("Enter divisibility test number: "))
+
+while True:
+  x = random.randint(1, 99999)
+  ans = raw_input("%d divisible by %d ? (y/n): " % (x, n)).lower()
+  while ans not in ['y','n']:
+    ans = raw_input("INVALID CHOICE !!\n%d divisible by %d ? (y/n): " % (x, n)).lower()
+  cnt += 1
+  if (x % n == 0):
+    if ans == 'y':
+      score += 1
+      print 'Correct !!'
+    elif ans == 'n':
+      score -= 1
+      print 'Wrong !!'
+  else:
+    if ans == 'y':
+      score -= 1
+      print 'Wrong !!'
+    elif ans == 'n':
+      score += 1
+      print 'Correct !!'
---
+++ squares_cubes.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+import signal
+import sys
+
+cnt = 0
+score = 0
+
+def signal_handler(signal, frame):
+  print '\n' + '-'*80
+  print 'score = %d/%d' % (score, cnt)
+  print 'Bye !'
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+header = 'SQUARE-CUBE TEST'
+print header
+print '-' * len(header)
+
+print '\nPress Ctrl+C to stop\n'
+
+while True:
+  x = random.randint(2, 25)
+  p = random.choice([2,3])
+  p_word = 'squared' if p == 2 else 'cubed'  
+  ans = raw_input('%d %s = ? ' % (x,p_word))
+  while not ans.isdigit():
+    ans = raw_input('INVALID CHOICE !!\n%d %s = ? ' % (x,p_word))
+  ans = int(ans)
+  cnt += 1
+  if ans == x**p:
+    print 'Correct !!'
+    score += 1
+  else:
+    print 'Wrong !!'
+    score -= 1
